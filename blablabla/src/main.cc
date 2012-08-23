72bc0ce3fc3b6f20afb998c8144b18dd6d8fbc23
Mateus Barros Rodrigues <x_fire_alchemist_x@hotmail.com>
2012-08-22 22:10:36 -0300

    Commented and organized both files

--- blablabla/src/main.cc
+++ blablabla/src/main.cc
@@ -1,6 +1,11 @@
+// Header
+#include "foca.h"
+#include "playerfoca.h"
+
+// External Deps
 #include <cstdio>
 #include <cstdlib>
-#include <ctime>
+//#include <ctime>
 
 #include <ugdk/action/task.h>
 #include <ugdk/action/scene.h>
@@ -9,10 +14,7 @@
 #include <ugdk/math/vector2D.h>
 #include <ugdk/input/inputmanager.h>
 
-
-#include "foca.h"
-#include "playerfoca.h"
-
+// Using
 using ugdk::Vector2D;
 using ugdk::action::Scene;
 
@@ -20,7 +22,7 @@ using ugdk::action::Scene;
 class EscTask : public ugdk::action::Task {
   public:
     EscTask(Scene* scene) : scene_(scene) {}
-    
+
     void operator() (double dt) {
         ugdk::input::InputManager* input = INPUT_MANAGER();
         if(input->KeyPressed(ugdk::input::K_ESCAPE)) {
@@ -32,18 +34,18 @@ class EscTask : public ugdk::action::Task {
 };
 
 int main(int argc, char *argv[]) {
-    srand(time(NULL));
-    
-    
+    //srand(time(NULL));
+
+
     ugdk::Configuration engine_config;
-    engine_config.window_title = "FOCAS - USPGAMEDEV 2012";  
-    engine_config.base_path = "/data";
-    
+    engine_config->window_title = "FOCAS - USPGAMEDEV 2012"; //Adicionei Setinhas =D
+    engine_config->base_path = "/data";
+
     ugdk::Engine* engine = ugdk::Engine::reference();
     engine->Initialize(engine_config);
 
     Scene* scene = new Scene;
-    scene->AddEntity(new PlayerFoca(50.0, 50.0));
+    scene->AddEntity(new PlayerFoca(300.0, 400.0)); //Mudei a pos. inicial da player foca for the lulz
     scene->AddEntity(new Foca(250.0, 80.0));
     scene->AddEntity(new Foca(550.0, 120.0));
     scene->AddTask(new EscTask(scene));
