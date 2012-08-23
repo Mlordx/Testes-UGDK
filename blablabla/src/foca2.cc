9a9b4ca841954e3dd9f7d9fdc99b9a8dac163ab3
Mateus Barros <x_fire_alchemist_x@hotmail.com>
2012-08-23 19:58:24 -0300

    Added 'true' collision and shit

---
+++ blablabla/src/foca2.cc
@@ -0,0 +1,70 @@
+
+// Header
+#include "foca.h"
+
+// External Deps
+#include <ugdk/action/entity.h>
+#include <ugdk/action/scene.h>
+#include <ugdk/base/engine.h>
+#include <ugdk/base/types.h>
+#include <ugdk/graphic/drawable.h>
+#include <ugdk/graphic/modifier.h>
+#include <ugdk/graphic/node.h>
+#include <ugdk/graphic/drawable/solidrectangle.h>
+#include <ugdk/math/vector2D.h>
+#include <ugdk/input/inputmanager.h>
+
+// Internal Deps
+
+// Using
+using ugdk::Color;
+using ugdk::Vector2D;
+using ugdk::action::Scene;
+using ugdk::graphic::Modifier;
+using ugdk::graphic::Node;
+using ugdk::graphic::SolidRectangle;
+using ugdk::input::InputManager;
+
+
+double randDouble() {
+    return rand() / static_cast<double>(RAND_MAX);
+}
+
+Foca::Foca(double x, double y) : velocity_(200, 0.0) {
+    SolidRectangle* solid = new SolidRectangle(Vector2D(50.0, 50.0));
+    solid->set_color(Color(randDouble(), randDouble(), randDouble(), 0.5));
+    node_ = new Node(solid);
+    node_->modifier()->set_offset(Vector2D(x, y));
+    node_->drawable()->set_hotspot(ugdk::graphic::Drawable::CENTER);
+}
+
+void Foca::Update(double dt) {
+    Modifier* modifier = node_->modifier();
+    Vector2D nova_pos = modifier->offset();
+    double pode_andar = 0.0;
+    
+    nova_pos += velocity_ * dt;
+    
+    
+    
+    if(nova_pos.x + node_->drawable()->width()/2.0 > 800) {
+        pode_andar = modifier->offset().x + node_->drawable()->width()/2.0 - 800;
+    
+        nova_pos.x += pode_andar - (velocity_.x * dt - pode_andar);
+        
+        velocity_.x *= -1;
+    } else if (nova_pos.x < 0.0 + node_->drawable()->width()/2.0) {
+        pode_andar = modifier->offset().x - node_->drawable()->width()/2.0;
+    
+        nova_pos.x += pode_andar - (velocity_.x * dt - pode_andar);
+    
+        velocity_.x *= -1;
+    }
+    
+    modifier->set_offset(nova_pos);
+}
+
+void Foca::OnSceneAdd(Scene* scene) {
+    scene->content_node()->AddChild(node_);
+}
+
