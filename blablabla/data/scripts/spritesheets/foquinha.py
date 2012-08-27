1ae7856608b3b07f1e7c7936d3e0b3ecdbdf7f55
Mateus Barros <x_fire_alchemist_x@hotmail.com>
2012-08-27 16:06:32 -0300

    Atualizei foquinha.py

--- blablabla/data/scripts/spritesheets/foquinha.py
+++ blablabla/data/scripts/spritesheets/foquinha.py
@@ -1,5 +1,5 @@
-import ugdk.ugdk_math
+import ugdk_math
 
 file = [ "images/Foquinha_v0.3.png" ]
-fill = [ 50, 50, ugdk.ugdk_math.Vector2D(25.0, 25.0) ]
+fill = [ 50, 50, ugdk_math.Vector2D(25.0, 25.0) ]
 
